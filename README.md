99378cf05b7985a08d1c5a55524abce887df9f7b
aragornzhao <aragornzhao@tencent.com>
2019-02-20 15:33:49 +0800

    新建工程

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # TiAndSiCmd
 计算视频ti及si信息，支持跨平台编译
+
+参考自项目 https://github.com/leixiaohua1020/TIandSI
+原项目只支持在windows环境下编译使用。
+修改使其可以跨平台。
+
+the project can used to be calculate si and ti index of yuv.
+refer to https://github.com/leixiaohua1020/TIandSI
+the original project can only be build in windows enviroment with visual studio
+modify original project then make it can be compiled on Linux
---
+++ TIAndSICmdForAll/TIandSIcmd.h
@@ -0,0 +1,25 @@
+/** 
+ * TIandSIcmd
+ *
+ * 王彩虹，雷霄骅，张晖
+ * nonmarking2010@gmail.com
+ * 中国传媒大学/数字电视技术
+ *
+ * 输入YUV文件路径，分辨率，像素格式
+ * 输出TI,SI值
+ */
+//#include "stdafx.h"
+//#include <iostream>
+#include <stdio.h>
+#include <memory.h>
+//#include <afx.h>
+#include <math.h> // add
+#include <string> // add
+#include <vector>
+#include <algorithm>
+// #include <atlstr.h>
+#include "XGetopt.h"
+//using std::vector;
+using namespace std;
+//#define _AFXDLL
+
